4526f8f7113e51243d588b0da9844e62bc382d76
Senlima Sun <senlima0430@gmail.com>
2020-06-05 21:41:54 +0800

    Add package and delete useless function

--- src/helper.ts
+++ src/helper.ts
@@ -4,15 +4,3 @@ export function generateId(): string {
     Math.random().toString(36).substring(2, 4)
   )
 }
-
-export function replaceItemAtIndex(
-  arr: { id: string; type: string; value: string; isEdit: boolean }[],
-  index: number,
-  newValue: { id: string; type: string; value: string; isEdit: boolean }
-) {
-  if (arr.length === 1) {
-    return [newValue]
-  }
-
-  return [...arr.slice(0, index), newValue, ...arr.slice(index + 1)]
-}
