18eac5539bab38e8c140b3849e97fee588cc5f26
Senlima Sun <senlima0430@gmail.com>
2020-06-06 16:56:52 +0800

    Add Article component and Update page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,12 @@
 import React from 'react'
-import { useRecoilValue } from 'recoil'
 import { RouteComponentProps } from '@reach/router'
-import { articleState } from '../hooks/atoms'
-import TextLine from '../components/TextLine'
+import Article from '../components/Article'
 
 function Home(props: RouteComponentProps) {
-  const article = useRecoilValue(articleState)
-
   return (
-    <article className="home__article markdown-body">
-      {article.map((line) => (
-        <TextLine
-          key={line.id}
-          id={line.id}
-          type={line.type}
-          value={line.value}
-          isEdit={line.isEdit}
-        />
-      ))}
-    </article>
+    <div style={{ position: 'relative', width: '100%' }}>
+      <Article />
+    </div>
   )
 }
 
