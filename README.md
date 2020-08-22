8ccf78119b867ec1b56bdfd5d5f8e136d4a0f027
Senlima Sun <senlima0430@gmail.com>
2020-08-23 06:02:06 +0800

    Setup
    Create react app typescript
    react app rewired config
    set path alias
    add public sources

--- README.md
+++ README.md
@@ -1,10 +1,3 @@
 # Artikolo
 
-> A text editor that interface like Medium's one
-
-<video id="video" autoplay="autoplay" preload="none">
-  <source src="https://github.com/senlima0430/artikolo/editor-1.mp4" type="video/mp4">
-</video>
-
-Build with Typescript React
-State management with Recoil and React hooks
+write article and manage to multiple platform
---
+++ src/theme.ts
@@ -0,0 +1,36 @@
+export default {
+  colors: {
+    primary: '#37F570',
+    secondary: '#9A49F5',
+  },
+  breakpoints: {
+    sm: '576px',
+    md: '768px',
+    lg: '1024px',
+    xl: '1280px',
+  },
+  fontSizes: {
+    small: 14,
+    medium: 16,
+    large: 18,
+    huge: 24,
+  },
+  inputs: {
+    size: {
+      default: {
+        fontSize: '16px',
+        lineHeight: '40px',
+        paddingLeft: '1em',
+        borderBottomWidth: '1px',
+        width: 'calc(100% - 1em)',
+      },
+      large: {
+        fontSize: '24px',
+        lineHeight: '60px',
+        paddingLeft: '16px',
+        borderBottomWidth: '2px',
+        width: 'calc(100% - 16px)',
+      },
+    },
+  },
+}
